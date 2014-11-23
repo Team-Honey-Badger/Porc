073676176a962fb78e7dca30265c26f40a6bc860
batmanasb <batmanasb@gmail.com>
2014-11-23 01:42:47 -0500

    Revert "commented out the contents of my w keymethod"
    This reverts commit c56a796ec4bc05be003136b25e6104bc44396c98.

--- GameApplication.cpp
+++ GameApplication.cpp
@@ -383,14 +383,14 @@ GameApplication::keyPressed( const OIS::KeyEvent &arg ) // Moved from BaseApplic
  //   }
 	else if (arg.key == OIS::KC_W)
 	{
-		//Agent *charAgent = NULL;
-		//std::list<Agent*>::iterator iter;
-		//for (iter = agentList.begin(); iter != agentList.end(); iter++)
-		//{
-		//	if ((*iter)->getAgentType() == 'c')
-		//		charAgent = (*iter);
-		//}
-		////charAgent->setOrientation(1);
+		std::list<Agent*>::iterator iter;
+		Agent *charAgent = NULL;
+		for (iter = agentList.begin(); iter != agentList.end(); iter++)
+		{
+			if ((*iter)->getAgentType() == 'c')
+				charAgent = (*iter);
+
+		}
 	}
 	else if (arg.key == OIS::KC_S)
 	{
