598f00f65e52fe030cfab4ec1c3fb6f8de1d1c6e
Noujou <gbencivengo4@gmail.com>
2014-12-03 00:48:15 -0500

    forgot () XD

--- Agent.cpp
+++ Agent.cpp
@@ -434,7 +434,7 @@ Agent::moveTo(){
 
 		if(mDirection == Ogre::Vector3::ZERO){ // only pick another location when not in motion
 			//wait until a turn comes up before switching directions
-			if (grid->isDone == true)
+			if (grid->isDone() == true)
 				while (1) { std::cout << "YOU WON!!!!!!!!! "; } 
 
 			switch(orientation)
