8ae176d169df179d62fc59559f50449d52464811
Noujou <gbencivengo4@gmail.com>
2014-12-03 00:11:05 -0500

    added map2

--- Agent.cpp
+++ Agent.cpp
@@ -59,8 +59,7 @@ Agent::Agent(Ogre::SceneManager* SceneManager, std::string name, std::string fil
 }
 
 Agent::~Agent(){
-	// mSceneMgr->destroySceneNode(mBodyNode); // Note that OGRE does not recommend doing this. It prefers to use clear scene
-	// mSceneMgr->destroyEntity(mBodyEntity);
+	//use clear scene
 }
 
 void
